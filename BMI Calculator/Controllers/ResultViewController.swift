5f530e30dfe24a32e17b3196bb49d9d769f655ff
jasonfangmagic <jasonfangmagic@gmail.com>
2024-03-14 20:00:04 -0400

    Add the Second View

---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,26 @@
+
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    var bmiValue: String?
+    
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bmiLabel.text = bmiValue
+    }
+    
+
+    
+    @IBAction func recalculatedPressed(_ sender: UIButton) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+}
