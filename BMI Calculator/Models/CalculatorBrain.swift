ce330ac5a50ffc484324a9f52ef768e875fc9434
jasonfangmagic <jasonfangmagic@gmail.com>
2024-03-15 17:16:24 -0400

    add advice

--- BMI Calculator/Models/CalculatorBrain.swift
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -6,24 +6,45 @@
 //  Copyright © 2024 Angela Yu. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 
 
 
 struct CalculatorBrain {
     
-    var bmi: Float?
+    var bmi:BMI?
     
     mutating func calculateBMI (_ height: Float, _ weight: Float ) {
         
-         bmi = weight/pow(height,2)
+        let bmiValue = weight/pow(height,2)
+        
+        if bmiValue < 18.5 {
+            bmi = BMI(value: bmiValue, advice: "UnderWeight", color: .blue)
+        }
+        else if bmiValue < 25 {
+            bmi = BMI(value: bmiValue, advice: "Normal", color: .green)
+        }
+        else {
+            bmi = BMI(value: bmiValue, advice: "Overweight", color: .red)
+        }
+        
         
     }
     
     func getBMIValue () -> String {
-        let bmiDecimal = String(bmi.rounded())
-        return bmiDecimal
+        let bmiDecimal = String(bmi?.value ?? Float(0.0).rounded())
+            return bmiDecimal
+    }
+    
+    func getAdvice () -> String {
+        let advice = bmi?.advice ?? "You're Dead"
+        return advice
+    }
+    
+    func getColor() -> UIColor{
+        let color = bmi?.color ?? .white
+        return color
     }
     
 }
